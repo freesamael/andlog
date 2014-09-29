513cfc354519d702c1d80d7c2bd104d993068eff
Samael Wang <freesamael@gmail.com>
2014-09-30 04:22:13 +0800

    Make LoggerFactory an interface.

--- src/org/andlog/LoggerFactory.java
+++ src/org/andlog/LoggerFactory.java
@@ -1,22 +1,19 @@
 
 package org.andlog;
 
-import android.text.TextUtils;
-
 /**
- * Abstract class of factories used to create {@link Logger} instances.
+ * Interface of factories used to create {@link Logger} instances.
  * 
  * @author samael_wang
  */
-public abstract class LoggerFactory {
-    protected static final String ANONYMOUS = "Anonymous";
+public interface LoggerFactory {
 
     /**
      * Create a {@link Logger} for the given {@code object}.
      * 
-     * @param obj {@link Object} of which the {@link Logger} works on.
-     *            Depending on the implementation, the object could be used to
-     *            generate the log tag, prefix or other values in the log.
+     * @param obj {@link Object} of which the {@link Logger} works on. Depending
+     *            on the implementation, the object could be used to generate
+     *            the log tag, prefix or other values in the log.
      * @return {@link Logger}
      */
     public abstract Logger create(Object obj);
@@ -24,28 +21,11 @@ public abstract class LoggerFactory {
     /**
      * Create a {@link Logger} for the given {@code clazz}.
      * 
-     * @param cls {@link Class} of which the {@link Logger} works on.
-     *            Depending on the implementation, the class could be used to
-     *            generate the log tag, prefix or other values in the log.
+     * @param cls {@link Class} of which the {@link Logger} works on. Depending
+     *            on the implementation, the class could be used to generate the
+     *            log tag, prefix or other values in the log.
      * @return {@link Logger}
      */
     public abstract Logger create(Class<?> cls);
 
-    /**
-     * Get the simple name of the {@code object}.
-     * 
-     * @param obj {@link Object} to find name. Must not be {@code null}.
-     * @return The type name of the object or {@value #ANONYMOUS} if type name
-     *         not available.
-     */
-    protected String getSimpleName(Object obj) {
-        String name;
-
-        // Get the object's type name, or its superclass's type name.
-        if (TextUtils.isEmpty(name = obj.getClass().getSimpleName())) {
-            name = obj.getClass().getSuperclass().getSimpleName();
-        }
-
-        return TextUtils.isEmpty(name) ? ANONYMOUS : name;
-    }
 }
--- src/org/andlog/SimpleLoggerFactory.java
+++ src/org/andlog/SimpleLoggerFactory.java
@@ -4,6 +4,8 @@ package org.andlog;
 import org.andlog.formatter.Formatter;
 import org.andlog.formatter.SimpleFormatter;
 
+import android.text.TextUtils;
+
 /**
  * The {@link Logger} created by {@link SimpleLoggerFactory} uses the type name
  * of the object / class it works on as log tag and simplest / fastest formatter
@@ -11,7 +13,8 @@ import org.andlog.formatter.SimpleFormatter;
  * 
  * @author samael_wang
  */
-public class SimpleLoggerFactory extends LoggerFactory {
+public class SimpleLoggerFactory implements LoggerFactory {
+    protected static final String ANONYMOUS = "Anonymous";
 
     @Override
     public Logger create(Object obj) {
@@ -30,13 +33,32 @@ public class SimpleLoggerFactory extends LoggerFactory {
     }
 
     /**
+     * Get the simple name of the {@code object}.
+     * 
+     * @param obj {@link Object} to find name. Must not be {@code null}.
+     * @return The type name of the object or {@value #ANONYMOUS} if type name
+     *         not available.
+     */
+    protected String getSimpleName(Object obj) {
+        String name;
+
+        // Get the object's type name, or its superclass's type name.
+        if (TextUtils.isEmpty(name = obj.getClass().getSimpleName())) {
+            name = obj.getClass().getSuperclass().getSimpleName();
+        }
+
+        return TextUtils.isEmpty(name) ? ANONYMOUS : name;
+    }
+
+    /**
      * Get the {@link Builder} instance to use.
      * 
      * @return {@link Builder}
      */
-    public Builder getBuilder() {
+    protected Builder getBuilder() {
         return new Builder(false/* extractMethodName */, new Formatter[] {
                 new SimpleFormatter()
         });
     }
+
 }
