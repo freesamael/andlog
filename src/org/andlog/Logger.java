b930e3ef1ccbbe3d0b67bf787fc144d1f08a71c7
samael_wang <samael_wang@htc.com>
2014-09-30 16:03:41 +0800

    Make the fields of Logger accessible by subclasses.

--- src/org/andlog/Logger.java
+++ src/org/andlog/Logger.java
@@ -10,9 +10,9 @@ import android.util.Log;
  * @author samael_wang
  */
 public class Logger {
-    private String mTag;
-    private String mPrefix;
-    private Builder mBuilder;
+    protected String mTag;
+    protected String mPrefix;
+    protected Builder mBuilder;
 
     public Logger(String tag, String prefix, Builder builder) {
         if (TextUtils.isEmpty(tag))
@@ -31,7 +31,6 @@ public class Logger {
      * @param objs {@link Object}s to log.
      * @see Log#v(String, String)
      */
-    @SuppressWarnings("unused")
     public void verbose(Object... objs) {
         if (BuildConfig.DEBUG || Log.isLoggable(mTag, Log.VERBOSE)) {
             String msg = mBuilder.build(mPrefix, objs);
@@ -46,7 +45,6 @@ public class Logger {
      * @param objs {@link Object}s to log.
      * @see Log#d(String, String)
      */
-    @SuppressWarnings("unused")
     public void debug(Object... objs) {
         if (BuildConfig.DEBUG || Log.isLoggable(mTag, Log.DEBUG)) {
             String msg = mBuilder.build(mPrefix, objs);
@@ -61,7 +59,6 @@ public class Logger {
      * @param objs {@link Object}s to log.
      * @see Log#i(String, String)
      */
-    @SuppressWarnings("unused")
     public void info(Object... objs) {
         if (BuildConfig.DEBUG || Log.isLoggable(mTag, Log.INFO)) {
             String msg = mBuilder.build(mPrefix, objs);
@@ -76,7 +73,6 @@ public class Logger {
      * @param objs {@link Object}s to log.
      * @see Log#w(String, String)
      */
-    @SuppressWarnings("unused")
     public void warning(Object... objs) {
         if (BuildConfig.DEBUG || Log.isLoggable(mTag, Log.WARN)) {
             String msg = mBuilder.build(mPrefix, objs);
@@ -91,7 +87,6 @@ public class Logger {
      * @param objs {@link Object}s to log.
      * @see Log#e(String, String)
      */
-    @SuppressWarnings("unused")
     public void error(Object... objs) {
         if (BuildConfig.DEBUG || Log.isLoggable(mTag, Log.ERROR)) {
             String msg = mBuilder.build(mPrefix, objs);
@@ -106,7 +101,6 @@ public class Logger {
      * @param objs {@link Object}s to log.
      * @see Log#wtf(String, String)
      */
-    @SuppressWarnings("unused")
     public void fatal(Object... objs) {
         if (BuildConfig.DEBUG || Log.isLoggable(mTag, Log.ASSERT)) {
             String msg = mBuilder.build(mPrefix, objs);
